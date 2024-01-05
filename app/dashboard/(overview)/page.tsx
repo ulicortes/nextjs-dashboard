8646800b9eebb643ed43d17e8125432e77a4fcc0
ulicortes <ulisestcortes@gmail.com>
2024-01-05 13:13:42 -0300

    Fin del capitulo 16 y finalizacion del curso.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,11 @@ import { lusitana } from '@/app/ui/fonts';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
  
 export default async function Page() {
   // const revenue = await fetchRevenue();
